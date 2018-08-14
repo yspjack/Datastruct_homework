15910221da0e3b119e392663139ce002b35b2912
yspjack <yspjack@gmail.com>
2018-08-14 14:20:41 +0800

    Homework 2 code

---
+++ code/homework2/example1c.c
@@ -0,0 +1,104 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+
+char expr[4096];
+int num[4096],top=0;
+int op[4096],top2=0;
+
+int isop(char ch)
+{
+    return ch=='+'||ch=='-'||ch=='*'||ch=='/';
+}
+void print_num()
+{
+    int i;
+    for(i=0; i<top; i++)
+    {
+        printf("%d ",num[i]);
+    }
+}
+void print_op()
+{
+    int i;
+    for(i=0; i<top2; i++)
+    {
+        printf("%c ",op[i]);
+    }
+}
+void process()
+{
+    char o=op[top2--];
+    int b=num[top--];
+    int a=num[top--];
+    if(o=='+')
+    {
+        num[++top]=a+b;
+    }
+    else if(o=='-')
+    {
+        num[++top]=a-b;
+    }
+    else if(o=='*')
+    {
+        num[++top]=a*b;
+    }
+    else if(o=='/')
+    {
+        num[++top]=a/b;
+    }
+}
+int main()
+{
+    fgets(expr,4096,stdin);
+    int i=0,n=strlen(expr);
+    while(i<n)
+    {
+        if(expr[i]<=' ')
+        {
+            ++i;
+            continue;
+        }
+        if(expr[i]=='*'||expr[i]=='/')
+        {
+            while(top2 && (op[top2]=='/'||op[top2]=='*'))
+            {
+                process();
+            }
+            op[++top2]=expr[i];
+        }
+        else if(expr[i]=='+'||expr[i]=='-')
+        {
+            while(top2 && isop(op[top2]))
+            {
+                process();
+            }
+            op[++top2]=expr[i];
+        }
+        else if(expr[i]>='0' && expr[i]<='9')
+        {
+            int x=0;
+            while(expr[i]>='0' && expr[i]<='9')
+            {
+                x=x*10+expr[i]-'0';
+                i++;
+            }
+            num[++top]=x;
+            continue;
+        }
+        else if(expr[i]=='=')
+        {
+            while(top2 && isop(op[top2]))
+            {
+                process();
+            }
+            break;
+        }
+        i++;
+    }
+    printf("%d\n",num[top]);
+    return 0;
+}
+
+
---
+++ code/homework2/pai.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <string.h>
+int A[15];
+int n;
+void dfs(int cur)
+{
+    if(cur==n)
+    {
+        int i;
+        for(i=0;i<n;i++)
+        {
+            printf("%d ",A[i]);
+        }
+        printf("\n");
+        return;
+    }
+    int i;
+    for(i=1;i<=n;i++)
+    {
+        int ok=1;
+        int j;
+        for(j=0;j<cur;j++)
+            if(A[j]==i)
+            {
+                ok=0;
+                break;
+            }
+        if(ok)
+        {
+            A[cur]=i;
+            dfs(cur+1);
+            A[cur]=0;
+        }
+    }
+}
+
+int main()
+{
+    scanf("%d",&n);
+    memset(A,0,sizeof(0));
+    dfs(0);
+    return 0;
+}
+
